8867a95290e1e9ac62d7a2eacbd94ba9d32b3b57
Francary <francarymolinapaez@gmail.com>
2025-06-14 13:14:19 -0300

    Se Agrego toUpperCase - Permite que los metodos no solo se escriban en mayuscula

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ import { postProductos } from './postProductos.js';
 
 const fakestoreapi = process.argv.slice(2);
 
-if (fakestoreapi[0] === 'GET' ) {
+if ( fakestoreapi[0].toUpperCase() === 'GET' ) {
 getProductos(fakestoreapi[1])
-} else if (fakestoreapi[0] === 'POST' ) {
+} else if (fakestoreapi[0].toUpperCase() === 'POST' ) {
 postProductos(fakestoreapi[1], fakestoreapi[2] , fakestoreapi[3],fakestoreapi[4])
-} else if (fakestoreapi[0] === 'DELETE' ) {
+} else if (fakestoreapi[0].toUpperCase() === 'DELETE' ) {
 deleteProductos(fakestoreapi[1])
 } else {
 console. log( `Comando no reconocido. Los Metodos Disponibles son GET , POST , DELETE.`)}
